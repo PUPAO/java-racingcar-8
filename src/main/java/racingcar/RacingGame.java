bb55eccd6b727b4b0a669c17684317d92c232429
PUPAO <tkdldjstm0127@gmail.com>
2025-10-25 11:41:06 +0900

    feat(move): 이동과 확인 기능 구현
    - 이동 후 차수 별 실행 결과를 출력함.

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RacingGame {
     private final InputDisplay inputDisplay = new InputDisplay();
     private final OutputDisplay outputDisplay = new OutputDisplay();
@@ -11,6 +13,27 @@ public class RacingGame {
         outputDisplay.askRoundNumber();
         int round = inputDisplay.getRound();
 
+        int[] distance = new int[racerList.length];
+
+        // 경기 중
+        while (round-- != 0) {
+            // 주사위
+            for (int i = 0; i < racerList.length; i++) {
+                int dice = Randoms.pickNumberInRange(0, 9);
+                if (dice >= 4) {
+                    distance[i]++;
+                }
+            }
+            // 경기 중 출력
+            for (int i = 0; i < racerList.length; i++) {
+                String dist = "";
+                if (distance[i] != 0) {
+                    dist = "-".repeat(distance[i]);
+                }
+
+                System.out.println(racerList[i] + " : " + dist);
+            }
+        }
         String[] winnerList = racerList;
         for(String i : winnerList)
             System.out.println("최종 우승자 : " + i);
